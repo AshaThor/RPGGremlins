bbd954c2b6170eb25016b3ace0dbd8b7ffdde696
AshaThor <smartyparty101@hotmail.com>
2021-01-20 00:09:40 +0000

    Alot of Css Changes BOOTSTRAP 5 PPL

---
+++ src/main/sql/data/class.sql
@@ -0,0 +1,7 @@
+INSERT INTO rpggremlins.class(id,
+                              name,
+                              hit_dice,
+                              character_skills_limit,
+                              lore)
+values (13, 'Artificer', 8, 2,
+        'Masters of invention, artificers use ingenuity and magic to unlock extraordinary capabilities in objects.')
