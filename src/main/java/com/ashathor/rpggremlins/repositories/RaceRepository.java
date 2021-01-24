c744d85417c94ab04f55fe3c23d4195e79cc4528
AshaThor <smartyparty101@hotmail.com>
2021-01-24 16:25:32 +0000

    Race Post Working

--- src/main/java/com/ashathor/rpggremlins/repositories/RaceRepository.java
+++ src/main/java/com/ashathor/rpggremlins/repositories/RaceRepository.java
@@ -1,6 +1,7 @@
 package com.ashathor.rpggremlins.repositories;
 
 import com.ashathor.rpggremlins.models.Race;
+import com.ashathor.rpggremlins.models.RpgClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RaceRepository extends JpaRepository<Race, Long> {
