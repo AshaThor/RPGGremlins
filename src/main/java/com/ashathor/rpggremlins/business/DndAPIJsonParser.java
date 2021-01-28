3a7e887cc0226f16dfeb57eb6db5d3238b102859
AshaThor <smartyparty101@hotmail.com>
2021-01-28 17:03:20 +0000

    Sonar

--- src/main/java/com/ashathor/rpggremlins/business/DndAPIJsonParser.java
+++ src/main/java/com/ashathor/rpggremlins/business/DndAPIJsonParser.java
@@ -6,8 +6,11 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.logging.Logger;
 
-public class JsonParcer {
+public class DndAPIJsonParser {
+
+    private static final Logger log = Logger.getLogger(DndAPIJsonParser.class.getName());
 
     public String run(String url) {
         RestTemplate restTemplate = new RestTemplate();
@@ -17,19 +20,14 @@ public class JsonParcer {
         Map<String,Object> map = springParser.parseMap(resp);
 
 
-        System.out.println("Items found: " + map.size());
         for (Map.Entry<String, Object> entry : map.entrySet()) {
-            if (entry.getKey() == "results"){
+            if (entry.getKey().equals("results")){
                 ArrayList<Object> list = (ArrayList<Object>) entry.getValue();
                 for(Object o : list) {
                     if(o instanceof Map) {
                         Map<String,Object> map2 = (Map<String,Object>) o;
-                        System.out.println("Items found: " + map2.size());
-
-                        int i = 0;
                         for (Map.Entry<String, Object> entry2 : map2.entrySet()) {
-                            System.out.println(entry2.getKey() + " = " + entry2.getValue());
-                            i++;
+                            log.info(entry2.getKey() + " = " + entry2.getValue());
                         }
 
                     }
--- src/main/java/com/ashathor/rpggremlins/controllers/JsonParserController.java
+++ src/main/java/com/ashathor/rpggremlins/controllers/JsonParserController.java
@@ -1,6 +1,6 @@
 package com.ashathor.rpggremlins.controllers;
 
-import com.ashathor.rpggremlins.business.JsonParcer;
+import com.ashathor.rpggremlins.business.DndAPIJsonParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class JsonParserController {
 
     @GetMapping
-    private String parce(){
-        JsonParcer jsonParcer = new JsonParcer();
+    public String parce(){
+        DndAPIJsonParser jsonParcer = new DndAPIJsonParser();
         return jsonParcer.run("https://www.dnd5eapi.co/api/classes");
 
     }
